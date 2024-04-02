5958c8f1462880b308640b9b098765fb4eb6b713
srikanthkarututi <skaruturi54@gmail.com>
2024-04-02 13:01:42 -0700

    new commit

--- backend.tf
+++ backend.tf
@@ -1,11 +1,19 @@
 terraform {
   backend "azurerm" {
   
-    subscription_id = "ADO_to_Azure_ServiceConnection"
+    /*subscription_id = "ADO_to_Azure_ServiceConnection"
     resource_group_name  = "rg-c2s-internal-srikanthk"
     storage_account_name = "srikanth2store"
     container_name       = "mystate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "prod.terraform.tfstate"*/
+
+    backendAzureRmResourceGroupName = "rg-c2s-internal-srikanthk"
+    backendServiceArm = "ADO_to_Azure_ServiceConnection"
+    backendAzureRmStorageAccountName = "srikanth2store"
+    backendAzureRmContainerName = "mystate"
+    backendAzureRmKey = "prod.terraform.tfstate"
+
+
   }
 }
 
