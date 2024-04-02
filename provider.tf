8b8b7961d63bda2df3cae0249126f1c032a717e8
srikanthkarututi <skaruturi54@gmail.com>
2024-04-01 18:51:28 -0700

    My-co

--- provider.tf
+++ provider.tf
@@ -11,8 +11,5 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "10618361-2f5f-485e-b2f1-84abf9e04a4f"
-  tenant_id       = "f5f02767-508e-4b78-bc05-2b16c8a081a1"
-  subscription_id = "f2556633-3f14-40c2-b1d7-da2e15f9977b"
 }
 
